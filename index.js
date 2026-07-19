c378b46adcf4dd928dcb3493165827674b64b856
barakangune <barakangune2@gmail.com>
2026-07-19 22:23:57 +0200

    Fix syntax error in index.js

--- index.js
+++ index.js
@@ -103,8 +103,8 @@ app.post("/webhook/moneroo", (req, res) => {
             success: false,
             message: "Erreur interne du serveur.",
         });
-    application.get('/', (req, res) => {
-    res.send('Mon serveur fonctionne!'}
+ application.get('/', (req, res) => {
+    res.send('Mon serveur fonctionne!'); }
 });
 
 app.listen(PORT, () => {
